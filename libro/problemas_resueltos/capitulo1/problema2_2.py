64356b362c0d4372fad9cf0a8c964de6620af6cc
AriRosell <54962255+AriRosell@users.noreply.github.com>
2019-09-29 16:39:15 -0500

    Tareas

---
+++ libro/problemas_resueltos/capitulo1/problema2_2.py
@@ -0,0 +1,9 @@
+SUE=float(input("Ingresa el sueldo: "))
+if SUE<1000:
+    AUM=SUE*0.15
+    NSUE=SUE+AUM
+    print(NSUE)
+else:
+    print("El nuevo sueldo es: ",SUE)
+print("Fin del programa")
+    
