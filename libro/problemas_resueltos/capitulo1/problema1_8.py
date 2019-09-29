ac3c59968a59f82e482b49f00d73777bfc39043a
AriRosell <54962255+AriRosell@users.noreply.github.com>
2019-09-29 15:47:49 -0500

    Add files via upload

---
+++ libro/problemas_resueltos/capitulo1/problema1_8.py
@@ -0,0 +1,6 @@
+X1=float(input("Variable de tipo Real X1:"))
+Y1=float(input("Variable de tipo Real Y1:"))
+X2=float(input("Variable de tipo Real X2:"))
+Y2=float(input("Variable de tipo Real Y2:"))
+DIS=((X1-X2)**2+(Y1-Y2)**2)**0.5
+print("Resultados:",DIS)
