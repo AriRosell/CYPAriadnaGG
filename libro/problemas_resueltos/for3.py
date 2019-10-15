291eb28cb344176e890ccb4ca0db984b1298e7f9
arirosell <arigallegos1999@gmail.com>
2019-10-15 17:36:05 -0500

    problemas resueltos for y while

---
+++ libro/problemas_resueltos/for3.py
@@ -0,0 +1,8 @@
+for v in range(3,31,3):
+    print("Hola",v)
+for v in range(20,-1,-1):
+    print("hola",v)
+print('------------------------------')
+VAL=int(input("Ingrese un valor mayor a cero:"))
+for v in range(0,VAL,1):
+    print("hola",VAL)
